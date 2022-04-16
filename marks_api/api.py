0cdb0e70b5c90f34d94470928d1a062bf2ba2cee
hks74123 <hemant91852@gmail.com>
2022-04-16 15:04:08 +0530

    added_comments

--- marks_api/api.py
+++ marks_api/api.py
@@ -3,12 +3,15 @@ import os
 import ast
 import requests
 
+# File to be sent through api
 file = 'Book - Sheet1.csv'
 
+# getting size of the file
 size = os.path.getsize(file)
 
 hash_md5 = hashlib.md5()
 
+# defining chunk size to be sent
 CHUNK_SIZE = 50
 
 start = 0
@@ -17,6 +20,7 @@ existingPath='null'
 with open(file, 'rb') as f:
     url = 'http://127.0.0.1:8000/chunkuplodation/'
     offset = 0
+    # Iterating in file in chunk portion
     for chunk in iter(lambda: f.read(CHUNK_SIZE), b''):
         hash_md5.update(chunk)
         uploadedChunk = start + CHUNK_SIZE
@@ -25,6 +29,7 @@ with open(file, 'rb') as f:
             end = 1
         else:
             end = 0
+        # Sending a Continous PUT request unit end of file. 
         res = requests.put(
             url,
             data={'filename': 'my_new_file','existingPath':existingPath,'end':end,'nextSlice':nextChunk},
--- marks_api/views.py
+++ marks_api/views.py
@@ -6,24 +6,31 @@ from rest_framework import generics
 import io, csv, pandas as pd
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import FileUploadSerializer,StudentSerializer  
-from .models import Student_data,Data_file
-from .exceptions import ChunkedUploadError
+from marks_api.serializers import FileUploadSerializer,StudentSerializer  
+from marks_api.models import Student_data,Data_file
 from django.http import JsonResponse   
 import os
 from django.conf import settings
 
 # Create your views here.
+
+# View for inserting data to database on uploading full file in a single chunk
 class InsertStudentData(generics.CreateAPIView):
     serializer_class = FileUploadSerializer
     
+    # view for handeling post request
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
+        #Validating file data
         file = serializer.validated_data['file']
+        
+        #reading file
         reader = pd.read_csv(file)
         save_file = Data_file(data_csv = request.FILES['file'])
-        save_file.save()  
+        save_file.save() 
+        #Saving Content of file to database 
         for _, row in reader.iterrows():
             new_file = Student_data(
                        id = row['id'],
@@ -36,24 +43,31 @@ class InsertStudentData(generics.CreateAPIView):
                         status.HTTP_201_CREATED)
     
 
+# View for Chunk uplodation
 class Get_chunk_files(generics.CreateAPIView):
     serializer_class = FileUploadSerializer
     
+    # Handeling the PUT requests
     def put(self, request, *args, **kwargs):
+        # Getting the details that are recived from api
         file = request.FILES['file'].read()
         fileName= request.POST['filename']
         existingPath = request.POST['existingPath']
         end = request.POST['end']
         nextSlice = request.POST['nextSlice']
 
+        # Validating the api data
         if file=="" or fileName=="" or existingPath=="" or end=="" or nextSlice=="":
             res = JsonResponse({'data':'Invalid Request'})
             return res
         else:
             if existingPath == 'null':
+                # Uploading and setting the path of file
                 path = 'media/' + fileName
                 with open(path, 'wb+') as destination: 
                     destination.write(file)
+                
+                # Creating a model instance of Data_file
                 FileFolder = Data_file()     
                 FileFolder.existingPath = fileName
                 FileFolder.eof = end
@@ -70,9 +84,11 @@ class Get_chunk_files(generics.CreateAPIView):
                 model_id = Data_file.objects.get(existingPath=existingPath)
                 if model_id.name == fileName:
                     if not model_id.eof:
+                        # Adding Chunks to existing file
                         with open(path, 'ab+') as destination: 
                             destination.write(file)
                         if int(end):
+                            # At end of the file saving it's path and details to database
                             model_id.data_csv = f'/{fileName}'
                             model_id.eof = int(end)
                             model_id.save()
